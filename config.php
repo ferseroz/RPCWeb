590636898bead468c35879a8563b349fc6f55ca4
Witt <ferseroz@hotmail.com>
2014-09-28 15:34:25 +0700

    commit

--- config.php
+++ config.php
@@ -4,6 +4,8 @@ $mysql_hostname = "localhost";
 $mysql_user = "root";
 $mysql_password = "";
 $mysql_database = "Raspberrypi";
+$SSH_USERNAME = 'hduser';
+$SSH_PASSWORD = 'ubuntu';
 $bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) 
 or die(mysql_error());
 mysql_select_db($mysql_database, $bd) or die(mysql_error());
--- maintable.php
+++ maintable.php
@@ -3,13 +3,27 @@
 	$result = mysql_query($query) or die(mysql_error());;
 
 	while($row = mysql_fetch_array($result)) {
-		echo "<tr>";
-		echo "<td>" . $row['nodename'] . "</td>";
-		echo "<td>" . $row['ip'] . "</td>";
-		echo "<td>" . $ssh->exec("top -b -n 10 -d.2 | grep 'Cpu' |  awk 'NR==3{ print($2)}'") . "</td>";
-		echo "<td> N/A </td>";
-		echo "<td> N/A </td>";
-		echo "<td><a href='http://" . $row['ip'] . ":4200' target='_blank'>" . $row['ip'] . "</a></td>";
-		echo "</tr>";
+		$host = $row['ip'];
+		$output = array();
+		$ping = null;
+		exec("ping -c 1 -s 8 -W 50 " . $host, $output, $ping);
+
+		if($ping == 0) {
+			echo "<tr>";
+			echo "<td>" . $row['nodename'] . "</td>";
+			echo "<td>" . $row['ip'] . "</td>";
+			echo "<td>" . $ssh->exec("top -b -n 10 -d.2 | grep 'Cpu' |  awk 'NR==3{ print($2)}'") . "</td>";
+			echo "<td> N/A </td>";
+			echo "<td> N/A </td>";
+			echo "<td><a href='http://" . $row['ip'] . ":4200' target='_blank'>" . $row['ip'] . "</a></td>";
+		} else {
+			echo "<tr>";
+			echo "<td>" . $row['nodename'] . "</td>";
+			echo "<td>" . $row['ip'] . "</td>";
+			echo "<td> N/A </td>";
+			echo "<td> N/A </td>";
+			echo "<td> N/A </td>";
+			echo "<td>Unavailable</td>";
+		}
 	}
 ?>
