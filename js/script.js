6563d8acfea445c0923d208b6247b1e52b1df3b7
Witt <ferseroz@hotmail.com>
2014-09-28 20:17:44 +0700

    Sync

--- js/script.js
+++ js/script.js
@@ -3,9 +3,19 @@ $(document).ready(function(){
 	// Hide all panes
 	var $panes = $('.Pane');
   	$panes.hide();
+  	var $nodecb = $("input[name='nodecb'");
+  	$nodecb.attr("disabled",true);
+
 
 	$("input[name='paneSelector']").click(function(){
 		$panes.hide();
 		$panes.eq($(this).val()).show();
 	});
+	$("select[name='headNode']").click(function(){
+
+
+
+	});
+
+
   });
