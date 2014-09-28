d60d0b10404808ad6c7a51cdd053bf1b4087ca98
tanniiz <tAnniiz@Pichests-MacBook-Air.local>
2014-09-29 04:16:36 +0700

    Get detail Table

--- getdetail.php
+++ getdetail.php
@@ -19,11 +19,13 @@ $query = "SELECT * FROM node WHERE ip='$ip'";
 		if($ping == 0) {
 			$ssh = new Net_SSH2($ip); 
 			if (!$ssh->login($SSH_USERNAME, $SSH_PASSWORD)) {
+				echo "Login Failed";
+			} else {
 				echo "<tr><th>CPU</th>";
 				echo "<td>" . $ssh->exec("top -b -n 10 -d.2 | grep 'Cpu' |  awk 'NR==3{ print($2)}'") . "</td></tr>";
 				echo "<tr><th>WORK</th>";
 				echo "<td>N/A</td></tr>";
-			} 
+			}
 		} else {
 				echo "<tr><th>CPU</th>";
 				echo "<td>N/A</td></tr>";
