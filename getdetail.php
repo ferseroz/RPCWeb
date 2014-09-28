9eb2403aca176de252ae18aa3aeaa6536a88551f
tanniiz <tAnniiz@Pichests-MacBook-Air.local>
2014-09-29 04:01:22 +0700

    Node Array

--- getdetail.php
+++ getdetail.php
@@ -12,17 +12,21 @@ $query = "SELECT * FROM node WHERE ip='$ip'";
 		echo "<tr><th>IP Address</th>";
 		echo "<td>" . $row['ip'] . "</td></tr>";
 		
-		$ssh = new Net_SSH2($ip);
-		if (!$ssh->login($SSH_USERNAME, $SSH_PASSWORD)) {
-	    	echo "<tr><th>CPU</th>";
-			echo "<td>N/A</td></tr>";
-			echo "<tr><th>WORK</th>";
-			echo "<td>N/A</td></tr>";
-		} else {
-			echo "<tr><th>CPU</th>";
-			echo "<td>" . $ssh->exec("top -b -n 10 -d.2 | grep 'Cpu' |  awk 'NR==3{ print($2)}'") . "</td></tr>";
-			echo "<tr><th>WORK</th>";
-			echo "<td>N/A</td></tr>";
+		try {
+			$ssh = new Net_SSH2($ip); 
+			if (!$ssh->login($SSH_USERNAME, $SSH_PASSWORD)) {
+		    	echo "<tr><th>CPU</th>";
+				echo "<td>N/A</td></tr>";
+				echo "<tr><th>WORK</th>";
+				echo "<td>N/A</td></tr>";
+			} else {
+				echo "<tr><th>CPU</th>";
+				echo "<td>" . $ssh->exec("top -b -n 10 -d.2 | grep 'Cpu' |  awk 'NR==3{ print($2)}'") . "</td></tr>";
+				echo "<tr><th>WORK</th>";
+				echo "<td>N/A</td></tr>";
+			}
+		} catch (Exception $e) {
+			echo "None";
 		}
 	}
 
