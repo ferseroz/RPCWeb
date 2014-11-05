81fdcc5d8ba87c52ead0f873bae237035f36bd50
tanniiz <tAnniiz@Pichests-MacBook-Air.local>
2014-11-05 07:12:31 +0700

    logs
    ip configuration log

--- ipconfigure.php
+++ ipconfigure.php
@@ -55,17 +55,51 @@ fclose($file);
 
 $sftp = new Net_SFTP($node);
 if (!$sftp->login($SSH_USERNAME, $SSH_PASSWORD)) {
-    exit('Login Failed');
+	exit('Login Failed');
 }
 
 if($sftp->put("/etc/network/interfaces", "upload/interfaces", NET_SFTP_LOCAL_FILE)) {
 	$ssh = new Net_SSH2($node);
 	if (!$ssh->login($SSH_USERNAME, $SSH_PASSWORD)) {
-	    exit('Login Failed');
+		exit('Login Failed');
 	}
 
 	$ssh->setTimeout(1);
 	$ssh->exec("sudo ifdown eth0; sudo ifup eth0");
+
+	if(!file_exists("logs/rpi_" . date("Ymd") . ".txt")){
+		$flog = "logs/rpi_" . date("Ymd") . ".txt";
+		$handle = fopen($flog, 'w');
+	}
+
+	$clientip = '';
+	if (getenv('HTTP_CLIENT_IP'))
+		$clientip = getenv('HTTP_CLIENT_IP');
+	else if(getenv('HTTP_X_FORWARDED_FOR'))
+		$clientip = getenv('HTTP_X_FORWARDED_FOR');
+	else if(getenv('HTTP_X_FORWARDED'))
+		$clientip = getenv('HTTP_X_FORWARDED');
+	else if(getenv('HTTP_FORWARDED_FOR'))
+		$clientip = getenv('HTTP_FORWARDED_FOR');
+	else if(getenv('HTTP_FORWARDED'))
+		$clientip = getenv('HTTP_FORWARDED');
+	else if(getenv('REMOTE_ADDR'))
+		$clientip = getenv('REMOTE_ADDR');
+	else
+		$clientip = 'UNKNOWN';
+
+	$log  = "IP: ".$clientip.' - '.date("F j, Y, g:i a").PHP_EOL.
+	$query = "Select * FROM node WHERE ip='$node'";
+	$result = mysql_query($query);
+	$log = $log . "Configure Node: ". $row['nodename'] . PHP_EOL;
+	$log = $log . "Old IP:" . $row['ip'] . PHP_EOL;
+	$log = $log . "New IP:" . $ip . PHP_EOL;
+	$log = $log . "Subnet Mask:" . $subnet . PHP_EOL;
+	$log = $log . "Gateway:" . $gateway . PHP_EOL;
+	$log = $log . "Network:" . $network . PHP_EOL;
+	$log = $log . "Broadcast:" . $broadcast . PHP_EOL;
+	$log = $log . "-------------------------".PHP_EOL;
+	file_put_contents("logs/rpi_" . date("Ymd") . ".txt", $log, FILE_APPEND);
 	header('Location: fetchnode.php');
 }
 
