1a351d739a4024919c2c92513a85e00cd71712f0
tanniiz <tAnniiz@Pichests-MacBook-Air.local>
2014-11-14 16:33:06 +0700

    ip

--- ssh.php
+++ ssh.php
@@ -5,6 +5,6 @@ $ping = null;
 
 exec("ping -n 4 -w 1000 " . $ip, $output, $result);
 if($result == 0) {
-	echo "<a href='#' id='ssh'>Available</a>";
+	echo "<a href='http://'" . $ip . ":4200' id='ssh'>Available</a>";
 }
 ?>
