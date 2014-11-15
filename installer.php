ee7bb301d1ef27e2b83eadcfc5646f5dab01b83f
tanniiz <tAnniiz@Pichests-MacBook-Air.local>
2014-11-16 03:06:08 +0700

    commit

--- installer.php
+++ installer.php
@@ -1,5 +1,6 @@
 <?php
 include('Net/SSH2.php');
+include('Net/SFTP.php');
 include('config.php');
 
 $ip = $_GET['ip'];
@@ -9,7 +10,30 @@ if (!$ssh->login($SSH_USERNAME, $SSH_PASSWORD)) {
     exit('Login Failed');
 }
 
-echo $ssh->exec("sudo halt");
+$dir = 'upload/System';
+$fileName = $_FILES['file']['name'];
+$tmpName = $_FILES['file']['tmp_name'];
+$fileSize = $_FILES['file']['size'];
+$fileType = $_FILES['file']['type'];
+
+$path = $dir. $fileName;
+
+if(move_uploaded_file($tmpName, $path)){
+	
+	$sftp = new Net_SFTP($node[$i]);
+	if (!$sftp->login($SSH_USERNAME, $SSH_PASSWORD)) {
+		exit('Login Failed');
+	}
+		
+	$locate = "" . $fileName;
+	$sftp->put($locate, $path, NET_SFTP_LOCAL_FILE);
+
+	$query = "Select * FROM node WHERE ip='$ip'";
+	$result = mysql_query($query);
+	$row = mysql_fetch_array($result);
+}
+
+echo $ssh->exec("sudo ./script");
 
 $query = "Select * FROM node WHERE ip='$ip'";
 $result = mysql_query($query);
@@ -38,11 +62,11 @@ else
 	$clientip = 'UNKNOWN';
 
 $log  = "IP: ".$clientip.' - '.date("F j, Y, g:i a").PHP_EOL.
-"Shutdown RaspberryPI(Halt)".PHP_EOL.
 "Node: ".$row['nodename']. " IP: ".$row['ip'].PHP_EOL;
+"has been installed and configured".PHP_EOL.
 $log = $log . "-------------------------".PHP_EOL;
 file_put_contents("logs/rpi_" . date("Ymd") . ".txt", $log, FILE_APPEND);
 
-header('Location: configuration.php');
+//header('Location: configuration.php');
 
 ?>
--- ssh.php
+++ ssh.php
@@ -3,8 +3,12 @@ $ip = $_GET['ip'];
 $output = array();
 $ping = null;
 
-exec("ping -n 4 -w 1000 " . $ip, $output, $result);
+//For Window
+//exec("ping -n 4 -w 1000 " . $ip, $output, $result);
+
+//For Mac
+exec("ping -c 1 -s 4 -W 500 " . $ip, $output, $result);
 if($result == 0) {
-	echo "<a href='http://'" . $ip . ":4200' id='ssh'>Available</a>";
+	echo "<a href='http://" . $ip . ":4200' id='ssh' target='blank' class='ssh'>Available</a>";
 }
 ?>
